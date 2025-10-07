83465e510808da6e3065659a9b8b6a150eb5b68d
Arthur <zthurzzz@gmail.com>
2025-10-07 20:14:04 -0300

    Aula 01

---
+++ aula01.py
@@ -0,0 +1,9 @@
+# Declare variaveis como nome endereço(Completo) telefone CPF
+# e imprima essas variaveis em um print 
+
+Nome = "Arthur"
+Endereco = "Estrada Manoel Lages do Chao 750"
+Telefone = "+55 (11)96296-2654"
+CPF = "549.718.248-80"
+
+print("\n", "Nome:", Nome, "\n",  "Endereço:", Endereco, "\n", "Telefone:", Telefone, "\n", "CPF:", CPF)
---
+++ teste.py
@@ -0,0 +1,4 @@
+idade = (int(input("Quantos anos vc tem?")))
+
+if idade >= 18:
+    print("Voce esta liberado para nossa balada")
